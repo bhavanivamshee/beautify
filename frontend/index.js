fe828c692d82847f51f29abec3bc0ecf9ccee526
Bhavani <bhavanivamshee@gmail.com>
2021-07-01 22:56:19 -0500

    adds dom content loaded event listener

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("click", (event)=>{ console.log("💻🔬👀:: You Just Clicked on == ", event.target) }  )
+document.addEventListener("DOMContentLoaded", function(){  console.log("WE, ARE,, LIVE🙆🏾‍♂️✨")
+
+
+
+
+
+
+
+
+})
