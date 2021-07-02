c6faa55a9e088632bdfc5a4fe787825190892ec4
Bhavani <bhavanivamshee@gmail.com>
2021-07-01 19:48:12 -0500

    adds :class_name to associations to fix carts_products

--- backend/app/models/cart.rb
+++ backend/app/models/cart.rb
@@ -1,4 +1,4 @@
 class Cart < ApplicationRecord
-    has_many :carts_products
+    has_many :carts_products, :class_name => 'CartsProducts'
     has_many :products, through: :carts_products
 end
--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    has_many :carts_products, 
+    has_many :carts_products, :class_name => 'CartsProducts'
     has_many :carts, through: :carts_products
 end
