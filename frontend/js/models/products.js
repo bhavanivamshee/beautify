f4ff56a56cfedfd9aaa9c1b145af6e80272928c0
Bhavani <bhavanivamshee@gmail.com>
2021-07-03 16:28:37 -0500

    adds products class and constructor

--- frontend/js/models/products.js
+++ frontend/js/models/products.js
@@ -1,3 +1,13 @@
 class Products {
 
+    constructor({title, description, price, quantity, category, id, product_image}) {
+        this.title = title; 
+        this.description = description;
+        this.price = price; 
+        this.quantity = quantity;
+        this.category = category;
+        this.id = id;
+        this.productImage = product_image;
+    }//products constructor
+
 }//end of products class
