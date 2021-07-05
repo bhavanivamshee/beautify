16fb91ac7dd399eb3a1416142213e652d612628b
Bhavani <bhavanivamshee@gmail.com>
2021-07-05 16:39:57 -0500

    adds orders_controller

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -1,16 +1,41 @@
 class OrdersController < ApplicationController
     def create
+        products = get_products
+        order = Order.create(
+            name: params[:name],
+            address: params[:address],
+            phone: params[:phone],
+            email: params[:email],
+            total: params[:total],
+            products: products
+        )
+        if order.valid?
+            render json: order, include: [products: {except: [:created_at, :updated_at]}] 
+        else
+            render json: order.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
-    def show
+
+    def destroy
+        order = Order.find_by(id: params[:id].to_i);
+        if order.destroy
+            render json: {status: :ok}
+        else
+            render json: {status: :unprocessable_entity}
+        end
     end
 
-    def edit
-    end
 
-    def update
-    end
+    private
 
-    def delete
+
+    def get_products
+        products = params[:products].map do |product|
+            i = Product.find(product[:id])
+            i.quantity = product[:quantity]
+            i
+        end
+        return products
     end
 end
